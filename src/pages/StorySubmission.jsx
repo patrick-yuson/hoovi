2454a9dcffedf17e4507023f89ee52f7fddacb18
Patrick Yuson <ntu7de@virginia.edu>
2025-04-21 12:54:31 -0400

    Added form for story submission and firebase config

--- src/pages/StorySubmission.jsx
+++ src/pages/StorySubmission.jsx
@@ -1,7 +1,44 @@
+import { useState } from "react";
+
 function StorySubmission() {
+    const [title, setTitle] = useState("");
+    const [text, setText] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log("Story title", title);
+        console.log("Story text", text);
+    }
+
     return (
         <>
-            <h1>This is where you can submit a story!</h1>
+            <h2>This is where you can submit a story!</h2>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label>Title:</label>
+                    <input
+                        type="text"
+                        value={title}
+                        onChange={(e) => setTitle(e.target.value)}
+                        required
+                    />
+                </div>
+                <div>
+                    <label>Story Text:</label>
+                    <br></br>
+                    <textarea
+                        value={text}
+                        onChange={(e) => setText(e.target.value)}
+                        rows={8}
+                        required
+                    />
+                </div>
+                <button
+                    type="submit"
+                >
+                    Submit
+                </button>
+            </form>
         </>
     )
 }
