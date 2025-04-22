7d54ed17d293d32485aa5b4bf8d1e3de61fe25ef
Patrick Yuson <ntu7de@virginia.edu>
2025-04-22 11:51:17 -0400

    Removed firestore and firebase auth implementations

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import './App.css';
 import Home from './pages/Home';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import StorySubmission from './pages/StorySubmission';
-import Login from './pages/Login';
 import Navbar from './components/Navbar';
 import Gallery from './pages/Gallery';
 import Stories from './pages/Stories';
@@ -16,7 +15,6 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/story-submission" element={<StorySubmission />} />
-          <Route path="/login" element={<Login />} />
           <Route path="/gallery" element={<Gallery />} />
           <Route path="/stories" element={<Stories />} />
         </Routes>
--- src/pages/StorySubmission.jsx
+++ src/pages/StorySubmission.jsx
@@ -1,76 +1,10 @@
-import { useState, useEffect } from "react";
-// import { collection, addDoc } from "firebase/firestore";
-import { auth } from "../../firebase.js"; // Add db
-import { onAuthStateChanged } from "firebase/auth";
+// import { useState } from "react";
 
 function StorySubmission() {
-    const [title, setTitle] = useState("");
-    const [text, setText] = useState("");
-    const [user, setUser] = useState(null);
-
-    useEffect(() => {
-        const unsubscribe = onAuthStateChanged(auth, (currUser) => {
-            setUser(currUser);
-        })
-
-        return () => unsubscribe();
-    }, []);
-
-    // RULES CURRENTLY DON'T ALLOW THIS
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        console.log("Story title", title);
-        console.log("Story text", text);
-
-        // try {
-        //     const docRef = await addDoc(collection(db, "stories"), {
-        //         title: title,
-        //         story: text,
-        //     })
-        //     console.log("Added document with ID: ", docRef.id);
-        // }
-        // catch (e) {
-        //     console.error("Error adding new story: ", e);
-        // }
-    }
 
     return (
         <>
             <h2>This is where you can submit a story!</h2>
-
-            {!user && (
-                <p>
-                    Please sign in to submit a story.
-                </p>
-            )}
-
-            <form onSubmit={handleSubmit}>
-                <div>
-                    <label>Title:</label>
-                    <input
-                        type="text"
-                        value={title}
-                        onChange={(e) => setTitle(e.target.value)}
-                        required
-                    />
-                </div>
-                <div>
-                    <label>Story Text:</label>
-                    <br></br>
-                    <textarea
-                        value={text}
-                        onChange={(e) => setText(e.target.value)}
-                        rows={8}
-                        required
-                    />
-                </div>
-                <button
-                    type="submit"
-                    disabled={!user}
-                >
-                    Submit
-                </button>
-            </form>
         </>
     )
 }
