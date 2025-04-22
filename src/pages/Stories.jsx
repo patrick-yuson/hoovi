636e60b37846d3fac545d88922f04941cca90bd9
Patrick Yuson <ntu7de@virginia.edu>
2025-04-22 14:13:40 -0400

    Added dark mode functionality

--- src/pages/Stories.jsx
+++ src/pages/Stories.jsx
@@ -6,8 +6,8 @@ import {
 function Stories() {
     return (
         <>
-            <Flex w="100vw" h="100vh" justifyContent="center" alignItems="center">
-                <Heading size="2xl">Here are the stories!</Heading>
+            <Flex _dark={{ bg: "var(--hh-paragraph)" }} w="100vw" h="100vh" justifyContent="center" alignItems="center" bg="var(--color-main)">
+                <Heading size="2xl" _dark={{ color: "var(--color-main)" }}>Here are the stories!</Heading>
             </Flex>
         </>
     )
